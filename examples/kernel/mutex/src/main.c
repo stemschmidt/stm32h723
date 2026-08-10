1d6b71c90f1af1f358978f3b8c766172ae2a2c94
Stefan Schmidt <kontakt@stefanschmidt-embedded.de>
2026-08-10 15:50:10 +0200

    launch config: remove dhcp4 sample,  add systemview translate semaphore example remove busy wait from mutex example

--- examples/kernel/mutex/src/main.c
+++ examples/kernel/mutex/src/main.c
@@ -1,7 +1,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
-// #define USE_BUSY_SLEEP
 // #define USE_MUTEX
 
 #define STACK_SIZE 1024
@@ -12,64 +11,38 @@
 K_THREAD_STACK_DEFINE(worker1_stack_area, STACK_SIZE);
 K_MUTEX_DEFINE(uart_mutex);
 
-static void custom_sleep(int ms)
-{
-#ifdef USE_BUSY_SLEEP
-    int64_t reftime = k_uptime_get();
-    int64_t diff = 0;
-    do
-    {
-        diff = k_uptime_get() - reftime;
-    } while (diff < (int64_t) ms);
-#else
-    k_sleep(K_MSEC(ms));
-#endif
-}
-
-void worker1(void *arg1, void *arg2, void *arg3)
-{
-    while (1)
-    {
+void worker1(void* arg1, void* arg2, void* arg3) {
+    while (1) {
 #ifdef USE_MUTEX
         k_mutex_lock(&uart_mutex, K_FOREVER);
 #endif
         printk("worker1: start ");
-        custom_sleep(500);
+        k_sleep(K_MSEC(500));
         printk("and end critical section\n");
 #ifdef USE_MUTEX
         k_mutex_unlock(&uart_mutex);
 #endif
-        custom_sleep(1000);
+        k_sleep(K_MSEC(1000));
     }
 }
 
-int main(void)
-{
+int main(void) {
     struct k_thread worker1_thread_data;
 
-    k_tid_t tid = k_thread_create(&worker1_thread_data,
-        worker1_stack_area,
-        K_THREAD_STACK_SIZEOF(worker1_stack_area),
-        worker1,
-        NULL,
-        NULL,
-        NULL,
-        WORKER1_PRIORITY,
-        0,
-        K_NO_WAIT);
+    k_tid_t tid = k_thread_create(&worker1_thread_data, worker1_stack_area, K_THREAD_STACK_SIZEOF(worker1_stack_area),
+                                  worker1, NULL, NULL, NULL, WORKER1_PRIORITY, 0, K_NO_WAIT);
     k_thread_name_set(tid, "worker1_id");
 
-    while (1)
-    {
+    while (1) {
 #ifdef USE_MUTEX
         k_mutex_lock(&uart_mutex, K_FOREVER);
 #endif
         printk("main: start ");
-        custom_sleep(200);
+        k_sleep(K_MSEC(200));
         printk("and end critical section\n");
 #ifdef USE_MUTEX
         k_mutex_unlock(&uart_mutex);
 #endif
-        custom_sleep(1000);
+        k_sleep(K_MSEC(1000));
     }
 }
--- examples/kernel/semaphore/src/main.c
+++ examples/kernel/semaphore/src/main.c
@@ -6,40 +6,36 @@
 
 static int sensor_value = 0;
 
-static void producer_device_callback(struct k_timer *dummy);
-static void device_work_handler(struct k_work *work);
+static void producer_device_callback(struct k_timer* dummy);
+static void device_work_handler(struct k_work* work);
 
-// Synchronisation von Interrupt (über WorkQueue) zu Thread
+/* Use semaphore to synchronize from interrupt via workqueue to thread. */
 K_SEM_DEFINE(data_ready, 0, 1);
 
-// Timer callback wird im Interrupt-Kontext aufgerufen
+/* timer callback will be invoked in interrupt context. */
 K_TIMER_DEFINE(producer_device, producer_device_callback, NULL);
-// Workqueue printk (nicht im Interrupt-Kontext aufrufen!)
+/* Workqueue: Don't call printk in interrupt context. */
 K_WORK_DEFINE(device_work, device_work_handler);
 
-void device_work_handler(struct k_work *work)
-{
-    printk("Erzeuger: Neuer Wert %d\n", sensor_value);
-    // Wecke Konsumenten
+void device_work_handler(struct k_work* work) {
+    printk("Producer: new value %d\n", sensor_value);
+    /* wake consumer. */
     k_sem_give(&data_ready);
 }
 
-static void producer_device_callback(struct k_timer *dummy)
-{
-    // Neuer Sensorwert!
+static void producer_device_callback(struct k_timer* dummy) {
+    /* new sensor value. */
     sensor_value++;
     k_work_submit(&device_work);
 }
 
-int main(void)
-{
+int main(void) {
     k_timer_start(&producer_device, K_MSEC(1000), K_MSEC(1000));
-    while (1)
-    {
-        // Warte auf Daten vom Produzenten
+    while (1) {
+        /* wait for data from producer. */
         k_sem_take(&data_ready, K_FOREVER);
 
-        printk("Konsument: Verarbeite Wert %d\n", sensor_value);
+        printk("Consumer: consume value %d\n", sensor_value);
     }
 
     return 0;
